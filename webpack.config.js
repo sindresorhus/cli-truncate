02fb1bba6a1f8683f0dc2df0b32c3c7a3653c8f5
Konstantin <thgala@gmail.com>
2017-05-11 14:27:13 +0200

    Transpile node_modules;

--- webpack.config.js
+++ webpack.config.js
@@ -12,12 +12,11 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /(node_modules)/,
         use: ['babel-loader']
       }
     ]
   },
   plugins: [
-    new UglifyJSPlugin()
+    // new UglifyJSPlugin()
   ],
 };
