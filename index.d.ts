ea9d7d733c1dd75ff656643899c500f31ddba4e7
Sindre Sorhus <sindresorhus@gmail.com>
2019-04-19 17:20:03 +0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -8,21 +8,23 @@ declare namespace cliTruncate {
 		readonly position?: 'start' | 'middle' | 'end';
 
 		/**
-		Add space between the text and the ellipsis.
+		Add a space between the text and the ellipsis.
+		
+		@default false
 
 		@example
 		```
-cliTruncate('unicorns', 5, {position: 'end', space: true});
-//=> 'uni …'
+		cliTruncate('unicorns', 5, {position: 'end', space: true});
+		//=> 'uni …'
 
-cliTruncate('unicorns', 5, {position: 'end', space: false});
-//=> 'unic…'
+		cliTruncate('unicorns', 5, {position: 'end', space: false});
+		//=> 'unic…'
 
-cliTruncate('unicorns', 6, {position: 'start', space: true});
-//=> '… orns'
+		cliTruncate('unicorns', 6, {position: 'start', space: true});
+		//=> '… orns'
 
-cliTruncate('unicorns', 7, {position: 'middle', space: true});
-//=> 'uni … s'
+		cliTruncate('unicorns', 7, {position: 'middle', space: true});
+		//=> 'uni … s'
 		```
 		*/
 		readonly space?: boolean;
